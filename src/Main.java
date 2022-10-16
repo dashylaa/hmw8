3c6038b124766d922c4ab5da428ca4eab1b713ba
dashylaa <112013267+dashylaa@users.noreply.github.com>
2022-10-16 20:03:42 +0500

    дз сделано

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        }
+
+    private static void task1() {
+    {
+        int [] numbers= new int[]{1,2,3};
+ numbers[0]=1;
+ numbers[1]=2;
+ numbers[2]=3;
+int number=0;
+System.out.println(numbers[number]);
+for (int i= numbers.length-1;i>=0;i--)
+{System.out.println(numbers[i]);}
+
+
+}}
+
+    private static void task2() {
+
+        double[] sm= { 1.57,7.654,9.986};
+        int number=0;
+        System.out.println(sm[number]);
+        for (int i= sm.length-1;i>=0;i--)
+        {System.out.println(sm[i]);}}
+
+        private static void task3() {
+
+            double[] ms= { 1.54,6.97,9.396};
+            int numb=0;
+            System.out.println(ms[numb]);
+            for (int i=0;i< ms.length;i++)
+            {System.out.println(ms[i]);}
+
+
+
+
+
+    }}
+
+
+
+
+
+
+
+
