80fc048b612198d92a7361f1f4f3ac40c9360ce2
dashylaa <112013267+dashylaa@users.noreply.github.com>
2022-10-17 19:38:54 +0500

    что-то не так в 4 задаче,не получается решить

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,56 @@ public class Main {
     }
 
     private static void task1() {
-    {
-        int [] numbers= new int[]{1,2,3};
- numbers[0]=1;
- numbers[1]=2;
- numbers[2]=3;
-int number=0;
-System.out.println(numbers[number]);
-for (int i= numbers.length-1;i>=0;i--)
-{System.out.println(numbers[i]);}
+        {
+            int[] numbers = new int[]{1, 2, 3};
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            int number = 0;
+            System.out.println(numbers[number]);
+            for (int i = numbers.length - 1; i >= 0; i--) {
+                System.out.println(numbers[i]);
+            }
 
 
-}}
+        }
+    }
 
     private static void task2() {
 
-        double[] sm= { 1.57,7.654,9.986};
-        int number=0;
+        double[] sm = {1.57, 7.654, 9.986};
+        int number = 0;
         System.out.println(sm[number]);
-        for (int i= sm.length-1;i>=0;i--)
-        {System.out.println(sm[i]);}}
+        for (int i = sm.length - 1; i >= 0; i--) {
+            System.out.println(sm[i]);
+        }
+    }
 
-        private static void task3() {
+    private static void task3() {
 
-            double[] ms= { 1.54,6.97,9.396};
-            int numb=0;
-            System.out.println(ms[numb]);
-            for (int i=0;i< ms.length;i++)
-            {System.out.println(ms[i]);}
-
-
+        double[] ms = {1.54, 6.97, 9.396};
+        int numb = 0;
+        System.out.println(ms[numb]);
+        for (int i = 0; i < ms.length; i++) ;
+    }
 
 
-
-    }}
-
+    private static void task4() {
+        int[] numbers = new int[]{1, 2, 3};
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        int number = 0;
+        System.out.println(numbers[number]);
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+            int[] array = new int[0];
+            if (array[i] % 2 != 0) {
+                array[i] = array[i] + 1;
+            }
+        }
+    }
+}
 
 
 
